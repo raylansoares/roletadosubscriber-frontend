80ae7cb7209c522300437794587c1fd0672c0ee7
Raylan Campos <raylan@raylansoares.com>
2020-06-21 16:27:59 -0300

    Add steps to config wheel

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -30,7 +30,67 @@
 
     <div id="how-section">
       <h2 class="title">Como Funciona</h2>
-      <p class="description">Em breve...</p>
+
+      <p class="description">
+        Sempre que seu canal receber uma nova inscrição, a roleta aparecerá 
+        automaticamente e irá selecionar aleatoriamente um dos prêmios
+        configurados por você. Veja o passo a passo:
+      </p>
+
+      <ul class="steps">
+        <li class="step">
+          <span class="step-number">1</span>
+          <span class="step-content">
+            Conecte sua conta da Twitch para acessar o painel.
+          </span>
+        </li>
+
+        <li class="step">
+          <span class="step-number">2</span>
+          <span class="step-content">
+            No painel, acesse a página de configurações clicando em
+            <strong>"Configurar Roleta"</strong> no menu.
+          </span>
+        </li>
+
+        <li class="step">
+          <span class="step-number">3</span>
+          <span class="step-content">
+            Cadastre ou edite os prêmios que aparecerão na roleta.
+          </span>
+        </li>
+
+        <li class="step">
+          <span class="step-number">4</span>
+          <span class="step-content">
+            No bloco <strong>"URL da Roleta para o OBS"</strong>, clique no campo
+            para exibir o endereço da roleta e em seguida copie o link exibido.
+          </span>
+        </li>
+
+        <li class="step">
+          <span class="step-number">5</span>
+          <span class="step-content">
+            No OBS, adicione uma nova fonte do tipo "Navegador/Browser" e cole
+            o link da roleta.
+          </span>
+        </li>
+
+        <li class="step">
+          <span class="step-number">6</span>
+          <span class="step-content">
+            Pronto! Assim que receber uma nova inscrição no seu canal a roleta
+            irá aparecer e selecionar um prêmio automaticamente.
+          </span>
+        </li>
+
+        <p class="description last">
+          Clicando no link <strong>Dashboard</strong> do painel, além de
+          visualizar a o histórico de inscrições e seus respectivos prêmios,
+          você também pode rodar a roleta manualmente para um usuário conforme
+          indicado no primeiro bloco da página.
+        </p>
+      </ul>
     </div>
 
     <div id="faq-section">
@@ -174,6 +234,7 @@ export default {
   justify-content: center;
   align-items: center;
   padding: 80px 0;
+  text-align: center;
   .title {
     font-weight: 300;
     font-size: 48px;
@@ -183,11 +244,40 @@ export default {
   }
   .description {
     color: #9fa4ad;
+    max-width: 1024px;
+    &.last {
+      padding-top: 30px;
+    }
   }
 }
 
 #how-section {
   background-color: #2d3748;
+  .steps {
+    max-width: 1024px;
+    padding: 20px 0;
+    list-style: none;
+    .step {
+      display: flex;
+      align-items: center;
+      padding: 5px 0;
+      color: #c9ccd2;
+      .step-number {
+        font-size: 22px;
+        padding: 0 10px;
+        height: 35px;
+        width: 35px;
+        border: 1px solid #c9ccd2;
+        background-color: #384456;
+        border-radius: 100px;
+        margin: 10px;
+      }
+      .step-content {
+        text-align: left;
+        font-size: 16px;
+      }
+    }
+  }
 }
 
 #faq-section {
